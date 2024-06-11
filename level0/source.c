64c9a3b2967afd4b068db13827a08bfd354e62fd
Jessy DAMOISEAU <jdamoise@bess-f2r1s20.clusters.42paris.fr>
2024-06-11 14:41:41 +0200

    update level0/source.c

--- level0/source.c
+++ level0/source.c
@@ -1,12 +1,8 @@
 #include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
-#include <stdio.h>
 
 int main(int ac, char **av){
-    char *arg = strdup("/bin/sh");
     if (atoi(av[1]) == 423){
-        execv("/bin/sh", &arg);
+        system("/bin/sh");
     }
     return 0;
 }
