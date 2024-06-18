866133f80776cd27e0f0fd67b2cc0c3f950e5611
Jessy DAMOISEAU <jdamoise@bess-f2r1s20.clusters.42paris.fr>
2024-06-18 13:12:12 +0200

    push

--- level8/source.c
+++ level8/source.c
@@ -4,12 +4,10 @@
 #include <stdlib.h>
 
 char *auth;
-int service;
+char *service;
 
 int main(){
-    char s[5];
-    char v5[2];
-    char v6[129];
+    char s[32];
 
     while (1){
         printf("%p, %p \n", auth, (const void *)service);
@@ -18,15 +16,15 @@ int main(){
         if (!memcmp(s, "auth ", 5)){
             auth = (char *)malloc(4);
             *auth = 0;
-            if (strlen(v5) <= 30)
-                strcpy(auth, v5);
+            if (strlen(s + 5) <= 30)
+                strcpy(auth, s + 5);
         }
         if (!memcmp(s, "reset", 5))
             free(auth);
         if (!memcmp(s, "service", 6))
-            service = (int)strdup(v6);
+            service = (int)strdup(s + 7);
         if (!memcmp(s, "login", 5)){
-            if (*auth + 8)
+            if (*auth + 32)
                 system("/bin/sh");
             else
                 fwrite("Password:\n", 1, 10, stdout);
